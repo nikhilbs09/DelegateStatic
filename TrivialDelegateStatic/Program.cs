c2ba5d28cb708221b6dfe0d66ed491ac6e948ee2
nikhil.singh <nikhil.singh@fulcrumbiometrics.com>
2019-12-17 15:48:35 +0530

    how to access a static and non-static method using a delegate.
    you don't need to create two different delegate type for both type of methods. Just one delegate is good to go as long as signature is same.

---
+++ TrivialDelegateStatic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrivialDelegateStatic
+{
+    delegate string StringPrinter(string paramString);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objTwo = new Two();
+            var printer = new StringPrinter(Two.StaticMethod);
+            Console.WriteLine(printer("koki-koki"));
+            Console.WriteLine("\n" + printer.Invoke("oki-doki"));
+
+            printer = new StringPrinter(objTwo.InstanceMethod);
+            Console.WriteLine(printer("TomDickHarry"));
+
+            Console.ReadLine();
+        }
+    }
+    class Two
+    {
+        public static string StaticMethod(string valStrOne)
+        {
+            return string.Format("Static Output: {0}", valStrOne);
+        }
+
+        public string InstanceMethod(string valStrTwo)
+        {
+            return string.Format("\nInstance Output: {0}", valStrTwo);
+        }
+    }
+}
